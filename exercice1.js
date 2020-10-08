3cc808d5256bf8e8002b20ee0409a73733cdd2dc
Marcin <m_a_r_c_i_n_@hotmail.com>
2020-10-08 15:32:19 +0200

    Changement scripts

--- exercice1.js
+++ exercice1.js
@@ -1,3 +1,38 @@
 function chuckNorris(msg) {
-  return "";
+  if(msg.length > 0 && msg.length < 100){
+    let binaryCode = "";
+    let unaryCode = "";
+    for(i in msg){
+    binaryCode += msg[i].charCodeAt(0).toString([2]);
+    }
+    let tableau = binaryCode.split("");
+    if (tableau[0] === '1'){
+      unaryCode += "0 0";
+    }
+    else{
+      unaryCode += "00 0"
+    }
+    for(let a = 1; a < tableau.length; a++){
+      if(tableau[a] === tableau[(a-1)]){
+        unaryCode += "0";
+      }
+      else{
+        if(tableau[a] === '1'){
+          unaryCode += " 0 0";
+        }
+        else{
+          unaryCode += " 00 0";
+        }
+      }
+    }
+    return unaryCode;
+  }
+  else{
+    return "Votre message est incorrect, veuillez réécrire un message entre 0 et 100 caractères.";
+  }
 }
+
+console.log(chuckNorris("C"));
+console.log(chuckNorris("CC"));
+
+
--- exercice2.js
+++ exercice2.js
@@ -1,3 +1,35 @@
 function analyzeTemperature(quantity, data) {
-  return 0;
+  if (quantity === 0){
+    return 0;
+  }
+  else if (quantity > 0 && quantity <= 10000){
+    let tempProcheZéro = 10000;
+    let newData = data.split(" ");
+    for (let i = 0; i < newData.length ; i++){
+      if (Number.parseInt(newData[i]) >= -273 && Number.parseInt(newData[i]) <= 5526){
+        nombre = Number.parseInt(newData[i]);
+        if(nombre < 0 && tempProcheZéro < 0 && nombre > tempProcheZéro){
+          tempProcheZéro = nombre;
+        }
+        else if (nombre < 0 && tempProcheZéro >= 0 && Math.abs(nombre) < tempProcheZéro){
+          tempProcheZéro = nombre;
+        }
+        else if (nombre >= 0 && tempProcheZéro >= 0 && nombre < tempProcheZéro){
+          tempProcheZéro = nombre;
+        }
+        else if (nombre >= 0 && tempProcheZéro < 0 && nombre < Math.abs(tempProcheZéro)){
+          tempProcheZéro = nombre;
+        }
+        else if (nombre > 0 && tempProcheZéro < 0 && nombre === Math.abs(tempProcheZéro)){
+          tempProcheZéro = nombre;
+        }
+      }
+    }
+    return tempProcheZéro;
+  }
+  else{
+    return "Le nombre de températures est incorrect, veuillez donner un nombre de températures entre 0 et 10000 inclus."
+  }
 }
+
+console.log(analyzeTemperature(5, "1 -2 -8 4 5"));
